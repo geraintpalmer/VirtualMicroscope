23022c4e05418ecac5fb90c63524e595646da336
geraint <palmer.geraint@googlemail.com>
2015-01-25 10:38:29 +0000

    Removed requirements.txt, added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 
+REQUIREMENTS = [
+	'Django==1.7.3',
+	'Pillow==2.7.0',
+	'sphinx_rtd_theme']
+
+
 setup(
     name='VirtualMicroscope',
     version='0.1dev',
     packages=['virtualmicroscope','nyuvm'],
     license='The MIT License (MIT)',
+    install_requires = REQUIREMENTS
     # long_description=open('README.txt').read(),
 )
