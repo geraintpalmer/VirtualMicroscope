07114077afb16aef534cfcef4a8e940e8cc74c5a
geraint <palmer.geraint@googlemail.com>
2015-01-25 13:57:40 +0000

    Added two small tests

--- tests/tests.py
+++ tests/tests.py
@@ -21,5 +21,33 @@ class NewVisitorTest(VisitorTest):
         # She checks that the title shows that she's in the correct place.
         self.assertIn('NYU School of Medicine Virtual Microscope', self.browser.title)
 
+    def test_can_get_login_page(self):
+        # Zoe wants to log in
+        # She opens up the browser at the homepage
+        self.browser.get('http://127.0.0.1:8000/virtualmicroscope/')
+        self.browser.implicitly_wait(3)
+
+        # She clicks on log in
+        link = self.browser.find_element_by_link_text('Login')
+        link.click()
+        self.assertEquals('http://127.0.0.1:8000/admin/login/?next=/admin/', self.browser.current_url)
+
+    def test_can_see_auraya(self):
+        # Zoe wants to view the auraya slide
+        # She opens the browser at the homepage
+        self.browser.get('http://127.0.0.1:8000/virtualmicroscope/')
+        self.browser.implicitly_wait(3)
+
+        # She clicks on Auraya from the dropdown menu Collections
+        dropdown = self.browser.find_elements_by_class_name('dropdown-toggle')
+        dropdown[0].click()
+        link = self.browser.find_element_by_link_text('auraya')
+        link.click()
+        self.assertEquals('http://127.0.0.1:8000/virtualmicroscope/collection/2/', self.browser.current_url)
+        link = self.browser.find_element_by_link_text('dog')
+        link.click()
+        self.assertEquals('http://127.0.0.1:8000/virtualmicroscope/v/2/?p=1', self.browser.current_url)
+
+
 if __name__ == '__main__':
     unittest.main()
