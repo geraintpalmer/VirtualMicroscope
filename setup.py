047969cad6241921e4f9adc9538300fbfa040326
geraint <palmer.geraint@googlemail.com>
2015-01-25 09:24:32 +0000

    Added a setup.py file, not sure of licence yet

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='VirtualMicroscope',
+    version='0.1dev',
+    packages=['virtualmicroscope','nyuvm'],
+    license='The MIT License (MIT)',
+    # long_description=open('README.txt').read(),
+)
